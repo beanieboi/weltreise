e499e50c2cfb2a5aca3ea8736d047cb115129b12
Kristin Fritsch <ich@kristin-fritsch.de>
2017-06-11 21:59:21 +0200

    Update marker copy

--- js/app/marker.js
+++ js/app/marker.js
@@ -4,13 +4,13 @@ define(function () {
       "title": "dddd",
       "position": new google.maps.LatLng(52.520007, 13.404954),
       "content": "<b>Tschüss Deutschland</b></br></br>" +
-        "-> <a href='#'>Blog post</a>"
+        "-> <a href='#' target='_blank'>Blog post</a>"
     },
      {
       "title": "dddd",
       "position": new google.maps.LatLng(4.710989, -74.072092),
       "content": "<b>Na sowas</b></br></br>" +
-        "-> <a href='#'>Blog post</a>"
+        "-> <a href='#' target='_blank'>Blog post</a>"
     }
   ]
 });
