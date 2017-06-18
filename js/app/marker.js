2242e0e7bdd8b2553bddb61987e200396106a4f0
Kristin Fritsch <ich@kristin-fritsch.de>
2017-06-18 16:44:32 +0200

    Reset content

--- js/app/marker.js
+++ js/app/marker.js
@@ -1,16 +1,16 @@
 define(function () {
   return [
-    {
-      "title": "dddd",
-      "position": new google.maps.LatLng(52.520007, 13.404954),
-      "content": "<b>Tschüss Deutschland</b></br></br>" +
-        "-> <a href='#' target='_blank'>Blog post</a>"
-    },
-     {
-      "title": "dddd",
-      "position": new google.maps.LatLng(4.710989, -74.072092),
-      "content": "<b>Na sowas</b></br></br>" +
-        "-> <a href='#' target='_blank'>Blog post</a>"
-    }
+    // {
+    //   "title": "dddd",
+    //   "position": new google.maps.LatLng(52.520007, 13.404954),
+    //   "content": "<b>Tschüss Deutschland</b></br></br>" +
+    //     "-> <a href='#' target='_blank'>Blog post</a>"
+    // },
+    //  {
+    //   "title": "dddd",
+    //   "position": new google.maps.LatLng(4.710989, -74.072092),
+    //   "content": "<b>Na sowas</b></br></br>" +
+    //     "-> <a href='#' target='_blank'>Blog post</a>"
+    // }
   ]
 });
--- js/app/travelledRoute.js
+++ js/app/travelledRoute.js
@@ -1,6 +1,19 @@
 define(function () {
   return [
     new google.maps.LatLng(52.520007, 13.404954), // TXL
-    new google.maps.LatLng(37.715727, -25.434506), // PDL
+    // new google.maps.LatLng(37.715727, -25.434506), // PDL
+    // new google.maps.LatLng(4.710989, -74.072092), // BOG
+    // new google.maps.LatLng(-11.838976, -76.751491), // LIM
+    // new google.maps.LatLng(-33.44889, -70.669266), // SCL
+    // new google.maps.LatLng(-34.603684, -58.381559), // EZE
+    // new google.maps.LatLng(-36.84846, 174.763332), // AUK
+    // new google.maps.LatLng(22.396428, 114.109497), // HKG
+    // new google.maps.LatLng(10.823099, 106.629664), // SGN
+    // new google.maps.LatLng(-6.17511, 106.86504), // JKT
+    // new google.maps.LatLng(35.689488, 139.691706), // NRT
+    // new google.maps.LatLng(22.396428, 114.109497), // HKG
+    // new google.maps.LatLng(12.971599, 77.594563), // BLR
+    // new google.maps.LatLng(31.945367, 35.928372), // AMM
+    // new google.maps.LatLng(52.520007, 13.404954) // TXL
   ]
 });
