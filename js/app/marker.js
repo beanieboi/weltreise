5e6104e21aaa68b2d6deb2522e645fbd1cb1b075
Kristin Fritsch <ich@kristin-fritsch.de>
2017-07-24 11:04:44 -0500

    Update wording

--- js/app/marker.js
+++ js/app/marker.js
@@ -5,7 +5,7 @@ define(function () {
       "position": new google.maps.LatLng(37.715727, -25.434506),
       "content": "<b>São Miguel (Azoren)</b><br/><br/>" +
       "Erfahre mehr über unsere Zeit<br/> auf São Miguel in unserem" +
-      "<a href='https://wir.abwesend.com/2017/07/weltreise-auftakt-sao-miguel-azoren/' target='_blank'> --> Blogbeitrag</a>"
+      "<a href='https://wir.abwesend.com/2017/07/weltreise-auftakt-sao-miguel-azoren/' target='_blank'> --> Blogpost</a>"
     },
     //  {
     //   "title": "dddd",
