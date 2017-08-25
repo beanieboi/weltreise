a950783996133f6052833fe638f51241935d4dea
Kristin Fritsch <ich@kristin-fritsch.de>
2017-08-24 19:35:40 -0500

    Add blog post for Colombia

--- js/app/marker.js
+++ js/app/marker.js
@@ -4,14 +4,17 @@ define(function () {
       "title": "São Miguel (Azoren)",
       "position": new google.maps.LatLng(37.715727, -25.434506),
       "content": "<b>São Miguel (Azoren)</b><br/><br/>" +
-      "Erfahre mehr über unsere Zeit<br/> auf São Miguel in unserem" +
-      "<a href='https://wir.abwesend.com/2017/07/weltreise-auftakt-sao-miguel-azoren/' target='_blank'> --> Blogpost</a>"
+      "Erfahre mehr über unsere Zeit<br/> auf São Miguel in " +
+      "<a href='https://wir.abwesend.com/2017/07/weltreise-auftakt-sao-miguel-azoren/' class='marker-link' target='_blank'>unserem Blog</a>.<br/><br/>" +
+      "<img src='https://wir.abwesend.com/wp-content/uploads/2017/08/DJI_0089_preview-1.jpg' width='200px' height='auto' class='marker-image'><br/>"
     },
-    //  {
-    //   "title": "dddd",
-    //   "position": new google.maps.LatLng(4.710989, -74.072092),
-    //   "content": "<b>Na sowas</b></br></br>" +
-    //     "-> <a href='#' target='_blank'>Blog post</a>"
-    // }
+    {
+      "title": "10 Highlights unserer Reise durch Kolumbien",
+      "position": new google.maps.LatLng(4.6323099, -74.0709709),
+      "content": "<b>10 Highlights unserer Reise durch Kolumbien</b></br></br>" +
+      "Erfahre mehr über unsere Zeit<br/> in Kolumbien in " +
+      "<a href='https://wir.abwesend.com/2017/08/10-highlights-unserer-reise-durch-kolumbien/' class='marker-link' target='_blank'>unserem Blog</a>.<br/><br/>" +
+      "<img src='https://wir.abwesend.com/wp-content/uploads/2017/08/IMG_8993_preview.jpg' width='200px' height='auto' class='marker-image'><br/>"
+    }
   ]
 });
