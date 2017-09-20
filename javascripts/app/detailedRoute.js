6611e5b3370a0d0e4b9678e78b1bc8db8d96725b
travis-deploy <travis-deploy@abwesend.com>
2017-09-20 23:39:19 +0000

    Site updated to 8e2b978

---
+++ javascripts/app/detailedRoute.js
@@ -0,0 +1,1 @@
+var detailed_route=[{location:new google.maps.LatLng(4.710989,-74.072092),stopover:!1},{location:new google.maps.LatLng(5.6365208,-73.5358133),stopover:!1},{location:new google.maps.LatLng(6.552555,-73.1374154),stopover:!1},{location:new google.maps.LatLng(6.6358495,-73.2277859),stopover:!1}];
