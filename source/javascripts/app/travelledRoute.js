a0f5c5bf8081b5a517d9e2ebfc87a7087ec3aeb9
ben <beanie@benle.de>
2017-10-11 07:52:00 +1300

    first night in the camper :D

--- source/javascripts/app/travelledRoute.js
+++ source/javascripts/app/travelledRoute.js
@@ -41,7 +41,8 @@ var travelled_route = [
     new google.maps.LatLng(-50.3283016,-72.3328393), // El Calafate, AR
     new google.maps.LatLng(-49.3278173,-72.9072553), // El Chalten, AR
     new google.maps.LatLng(-41.133814, -71.297070), // Bariloche, AR
-    new google.maps.LatLng(-36.8651467, 174.7562617), // AKL
+    new google.maps.LatLng(-36.8651467, 174.7562617), // AKL, NZ
+    new google.maps.LatLng(-37.959123, 175.290566), // Lake, Ngaroto, NZ
     // new google.maps.LatLng(22.396428, 114.109497), // HKG
     // new google.maps.LatLng(10.823099, 106.629664), // SGN
     // new google.maps.LatLng(-6.17511, 106.86504), // JKT
