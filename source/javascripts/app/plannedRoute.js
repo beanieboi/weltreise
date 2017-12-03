50974857c47e35fa507ada7458e2d3fc783888c5
ben <beanie@benle.de>
2017-12-03 11:34:15 +0700

    Remove SGN from destinations

--- source/javascripts/app/plannedRoute.js
+++ source/javascripts/app/plannedRoute.js
@@ -8,7 +8,7 @@ var planned_route = [
     // new google.maps.LatLng(-34.603684, -58.381559), // EZE
     // new google.maps.LatLng(-36.84846, 174.763332), // AUK
     // new google.maps.LatLng(22.396428, 114.109497), // HKG
-    new google.maps.LatLng(35.689488, 139.691706), // NRT
+    // new google.maps.LatLng(35.689488, 139.691706), // NRT
     new google.maps.LatLng(10.823099, 106.629664), // SGN
     new google.maps.LatLng(-6.17511, 106.86504), // JKT
     new google.maps.LatLng(22.396428, 114.109497), // HKG
