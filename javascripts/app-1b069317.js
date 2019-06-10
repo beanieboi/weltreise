3746d070e043e62ceb60fd8ba35c36da3c0bf727
travis-deploy <travis-deploy@abwesend.com>
2019-06-10 09:20:28 +0000

    Site updated to dcda220

---
+++ javascripts/app-1b069317.js
@@ -0,0 +1,1 @@
+!function(){document.addEventListener("DOMContentLoaded",function(){Feed.init(),MyMap.init()}),window.onScrollFeed=Feed.onScroll,window.addEventListener("scroll",Feed.onScroll),window.addEventListener("resize",MyMap.resize),window.centerAndZoomMapTo=MyMap.centerAndZoomMapTo,window.resetMap=MyMap.resetMap}();
--- javascripts/app-5fe9ae9b.js
+++
@@ -1,1 +0,0 @@
-!function(){document.addEventListener("DOMContentLoaded",function(){Feed.init(),Map.init()}),window.onScrollFeed=Feed.onScroll,window.addEventListener("scroll",Feed.onScroll),window.addEventListener("resize",Map.resize),window.centerAndZoomMapTo=Map.centerAndZoomMapTo,window.resetMap=Map.resetMap}();
